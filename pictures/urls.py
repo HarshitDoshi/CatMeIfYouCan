7636c33c6170688b63a48888210a32783bfde1fe
Harshit Doshi <5103046+HarshitDoshi@users.noreply.github.com>
2022-11-09 18:47:43 +0000

    Lint The Source Code (using black)

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,18 +1,12 @@
 from django.urls import path
 from pictures.views import CatPictureViewSet
 
-cat_picture_list = CatPictureViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-cat_picture_detail = CatPictureViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
+cat_picture_list = CatPictureViewSet.as_view({"get": "list", "post": "create"})
+cat_picture_detail = CatPictureViewSet.as_view(
+    {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
+)
 
 urlpatterns = [
-    path('', cat_picture_list, name='cat_picture-list'),
-    path('<int:pk>/', cat_picture_detail, name='cat_picture-detail'),
+    path("", cat_picture_list, name="cat_picture-list"),
+    path("<int:pk>/", cat_picture_detail, name="cat_picture-detail"),
 ]
